cc910aace1ad428240648c74de0ad072ae1199d4
j314h <johnhaimez314@gmail.com>
2021-02-12 14:05:38 +0100

    update

--- app/models/Trades.js
+++ app/models/Trades.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tradeSchema = Schema(
   {
     libelle: String,
-    company: { type: Schema.Types.ObjectId, ref: 'companies', autopopulate: true },
+    famille: String,
+    company: { type: Schema.Types.ObjectId, ref: "companies", autopopulate: true },
   },
   {
     timestamps: true,
   }
 );
 
-tradeSchema.plugin(require('mongoose-autopopulate'));
+tradeSchema.plugin(require("mongoose-autopopulate"));
 
-const Trades = mongoose.model('trades', tradeSchema);
+const Trades = mongoose.model("trades", tradeSchema);
 
 module.exports = Trades;
