0a48b8a043482810f0693aad8823de42dfb7d613
j314h <johnhaimez314@gmail.com>
2021-02-14 09:37:16 +0100

    update

--- app/models/BusinessRequiredSkills.js
+++ app/models/BusinessRequiredSkills.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const businessRequiredSkillSchema = Schema(
   {
     libelle: String,
+    description: String,
     ponderation: Number,
-    level: { type: Schema.Types.ObjectId, ref: 'level_skills', autopopulate: true },
-    trade: { type: Schema.Types.ObjectId, ref: 'trades', autopopulate: true },
-    user: { type: Schema.Types.ObjectId, ref: 'users', autopopulate: true },
+    level: { type: Schema.Types.ObjectId, ref: "level_skills", autopopulate: true },
+    trade: { type: Schema.Types.ObjectId, ref: "trades", autopopulate: true },
   },
   {
     timestamps: true,
   }
 );
 
-businessRequiredSkillSchema.plugin(require('mongoose-autopopulate'));
+businessRequiredSkillSchema.plugin(require("mongoose-autopopulate"));
 
-const BusinessRequiredSkills = mongoose.model('business_required_skills', businessRequiredSkillSchema);
+const BusinessRequiredSkills = mongoose.model("business_required_skills", businessRequiredSkillSchema);
 
 module.exports = BusinessRequiredSkills;
